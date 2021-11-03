3b2d0b3cb03f0bb440d90778b7632d7b160d55e0
Rafaela Brandão Mania Lima <60527114+Rafaela-create013@users.noreply.github.com>
2021-11-03 20:20:27 -0300

    novos arquivos

---
+++ react.php
@@ -0,0 +1,6 @@
+<?php
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+echo "O nome é $nome e a idade é $idade"
+?>
---
+++ teste.php
@@ -0,0 +1,9 @@
+<hmtl>
+    <body>
+        <form action="react.php" method="POST">
+        <input type="text" name="nome">
+        <input type="number" name="idade">
+        <input type="submit" name="enviar">
+        </form>
+    </body>
+</hmtl>
