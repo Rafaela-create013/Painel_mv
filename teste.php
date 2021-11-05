780c60fb54425949a6c8cab62cf7daa2c1f2fe94
Rafaela Brandão Mania Lima <60527114+Rafaela-create013@users.noreply.github.com>
2021-11-05 14:07:50 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,9 +0,0 @@
-<hmtl>
-    <body>
-        <form action="react.php" method="POST">
-        <input type="text" name="nome">
-        <input type="number" name="idade">
-        <input type="submit" name="enviar">
-        </form>
-    </body>
-</hmtl>
